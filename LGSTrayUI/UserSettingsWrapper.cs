436064f4c321ad0483db6911da3088c3d281f064
Sinan AKYAZICI <sinan_akyazici@hotmail.com>
2026-09-08 14:04:42 +0300

    Update LGSTrayUI/UserSettingsWrapper.cs

--- LGSTrayUI/UserSettingsWrapper.cs
+++ LGSTrayUI/UserSettingsWrapper.cs
@@ -1,4 +1,4 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
 using System.Collections.Specialized;
 
 namespace LGSTrayUI
@@ -7,6 +7,50 @@ namespace LGSTrayUI
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "")]
         public StringCollection SelectedDevices => Properties.Settings.Default.SelectedDevices;
+
+        public DeviceNumericColors GetDeviceNumericColors(string deviceId) =>
+            DeviceNumericColorStore.Get(Properties.Settings.Default.DeviceNumericColors, deviceId);
+
+        public void SetDeviceNumericColors(string deviceId, DeviceNumericColors colors)
+        {
+            Properties.Settings.Default.DeviceNumericColors = DeviceNumericColorStore.Set(
+                Properties.Settings.Default.DeviceNumericColors, deviceId, colors);
+            Properties.Settings.Default.Save();
+            OnPropertyChanged("DeviceNumericColors");
+        }
+
+        public bool NumericBold
+        {
+            get => Properties.Settings.Default.NumericBold;
+            set
+            {
+                Properties.Settings.Default.NumericBold = value;
+                Properties.Settings.Default.Save();
+                OnPropertyChanged();
+            }
+        }
+
+        public string NumericTextColor
+        {
+            get => Properties.Settings.Default.NumericTextColor;
+            set
+            {
+                Properties.Settings.Default.NumericTextColor = value;
+                Properties.Settings.Default.Save();
+                OnPropertyChanged();
+            }
+        }
+
+        public string NumericBackgroundColor
+        {
+            get => Properties.Settings.Default.NumericBackgroundColor;
+            set
+            {
+                Properties.Settings.Default.NumericBackgroundColor = value;
+                Properties.Settings.Default.Save();
+                OnPropertyChanged();
+            }
+        }
 
         public bool NumericDisplay
         {
@@ -42,3 +86,4 @@ namespace LGSTrayUI
         }
     }
 }
+
